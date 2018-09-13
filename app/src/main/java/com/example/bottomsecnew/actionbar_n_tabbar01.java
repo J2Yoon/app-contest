56812f7d029089102c9cea7bf5684ac33a698c87
J2Yoon <42733881+J2Yoon@users.noreply.github.com>
2018-09-13 20:03:25 +0900

    test

---
+++ app/src/main/java/com/example/bottomsecnew/actionbar_n_tabbar01.java
@@ -0,0 +1,94 @@
+package com.example.bottomsecnew;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class actionbar_n_tabbar01 extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    android.support.v7.app.ActionBar bar;
+    private FragmentManager fm;
+    private ArrayList<Fragment> fList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actionbar_n_tabbar01);
+
+        // 스와이프할 뷰페이저를 정의
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+
+        // 프라그먼트 매니져 객체 정의
+        fm = getSupportFragmentManager();
+
+        // 액션바 객체 정의
+        bar = getSupportActionBar();
+
+        // 액션바 속성 정의
+        bar.setDisplayShowTitleEnabled(true);   // 액션바 노출 유무
+        bar.setTitle("Actionbar Tab Sample");   // 액션바 타이틀 라벨
+
+        // 액션바에 모드 설정 = ActionBar.NAVIGATION_MODE_TABS 로 TAB 모드로 설정
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // 액션바에 추가될 탭 생성
+        ActionBar.Tab tab1 = bar.newTab().setText("최신순").setTabListener(tabListener);
+        ActionBar.Tab tab2 = bar.newTab().setText("인기순").setTabListener(tabListener);
+
+
+        // 액션바에 탭 추가
+        bar.addTab(tab1);
+        bar.addTab(tab2);
+
+
+        // 각 탭에 들어갈 프라그먼트 생성 및 추가
+        fList = new ArrayList<Fragment>();
+        fList.add(fragment1.newInstance());
+        fList.add(fragment2.newInstance());
+
+        // 스와이프로 탭간 이동할 뷰페이저의 리스너 설정
+        mViewPager.setOnPageChangeListener(viewPagerListener);
+
+        // 뷰페이져의 아답터 생성 및 연결
+        CustomFragmentPagerAdapter adapter = new CustomFragmentPagerAdapter(fm, fList);
+        mViewPager.setAdapter(adapter);
+
+    }
+
+    ViewPager.SimpleOnPageChangeListener viewPagerListener = new ViewPager.SimpleOnPageChangeListener(){
+        @Override
+        public void onPageSelected(int position) {
+            super.onPageSelected(position);
+
+            // 뷰페이저 이동시 해당 탭으로 이동
+            bar.setSelectedNavigationItem(position);
+        }
+    };
+
+
+    ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+        @Override
+        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+            // 해당 탭에서 벚어났을때 처리
+        }
+
+        @Override
+        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+            // 해당 탭을 선택시 처리
+            // 해당 탭으로 뷰페이저도 이동
+            mViewPager.setCurrentItem(tab.getPosition());
+        }
+
+        @Override
+        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+            // 해당 탭이 다시 선택됐을때 처리
+        }
+    };
+}
---
+++ app/src/main/java/com/example/bottomsecnew/fragment1.java
@@ -0,0 +1,30 @@
+package com.example.bottomsecnew;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragment1 extends Fragment {
+
+    public static fragment1 newInstance(){
+        fragment1 fragment = new fragment1();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment1,container,false);
+
+        return view;
+    }
+}
